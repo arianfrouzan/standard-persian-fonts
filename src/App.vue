0e10aff447db3fcaa1d93af75d3358ecdb85fe7d
Sohrab <sohrab.taee@gmail.com>
2017-07-14 00:55:29 +0430

    add: font manufacturer link

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,10 @@
                     <div class="panel panel-default" v-for="font in filteredFonts" :key="font.nameEn"
                          :class="font.nameEn | slug">
                         <div class="panel-heading clearfix">
-                            <h3 class="pull-right panel-title">{{font.nameFa}}</h3>
-                            <h3 class="pull-left panel-title">{{font.nameEn}}</h3>
+                            <h3 class="pull-right panel-title">{{font.nameFa}} ({{font.nameEn}})</h3>
+                            <h3 class="pull-left panel-title" v-if="font.url">
+                                <a :href="font.url" target="_blank">صفحه سازنده</a>
+                            </h3>
                         </div>
                         <div class="row">
                             <div :class="font.weights.length > 1 ? 'col-lg-6' : 'col-xs-12'"
@@ -92,84 +94,98 @@
                     {
                         nameFa: 'بی‌بی‌سی نسیم',
                         nameEn: 'BBC Nassim',
+                        url: 'https://www.rosettatype.com/custom-fonts/BBC-Nassim',
                         type: 'serif',
                         weights: ['normal', 'bold']
                     },
                     {
                         nameFa: 'دروید نسخ',
                         nameEn: 'Droid Naskh',
+                        url: 'https://fontlibrary.org/en/font/droid-arabic-naskh',
                         type: 'serif',
                         weights: ['normal', 'bold']
                     },
                     {
                         nameFa: 'گندم',
                         nameEn: 'Gandom',
+                        url: 'https://github.com/rastikerdar/gandom-font',
                         type: 'serif',
                         weights: ['normal', 'bold']
                     },
                     {
                         nameFa: 'گنج نامه',
                         nameEn: 'Ganj Nameh',
+                        url: 'https://github.com/font-store/GanjnamehFont',
                         type: 'sans-serif',
                         weights: ['normal']
                     },
                     {
                         nameFa: 'ایران سنس',
                         nameEn: 'Iran Sans',
+                        url: 'http://fontiran.com/%D8%AE%D8%A7%D9%86%D9%88%D8%A7%D8%AF%D9%87-%D9%81%D9%88%D9%86%D8%AA-%D8%A7%DB%8C%D8%B1%D8%A7%D9%86-%D8%B3%D9%86-%D8%B3%D8%B1%DB%8C%D9%81-iran-sans-%D9%BE%D9%86%D8%AC-%D9%88%D8%B2%D9%86-%D9%87%D9%85/',
                         type: 'sans-serif',
                         weights: ['ultra-light', 'light', 'normal', 'medium', 'bold']
                     },
                     {
                         nameFa: 'ایران یکان',
                         nameEn: 'Iran Yekan',
+                        url: 'http://fontiran.com/%D9%81%D9%88%D9%86%D8%AA-%D8%A7%DB%8C%D8%B1%D8%A7%D9%86-%DB%8C%DA%A9%D8%A7%D9%86-iran-yekan/',
                         type: 'sans-serif',
                         weights: ['light', 'normal', 'bold']
                     },
                     {
                         nameFa: 'میلاد آزاد',
                         nameEn: 'Milad Azad',
+                        url: 'https://github.com/font-store/font-MiladAzad',
                         type: 'serif',
                         weights: ['normal']
                     },
                     {
                         nameFa: 'پرستو',
                         nameEn: 'Parastoo',
+                        url: 'https://github.com/rastikerdar/parastoo-font',
                         type: 'serif',
                         weights: ['normal', 'bold']
                     },
                     {
                         nameFa: 'ساحل',
                         nameEn: 'Sahel',
+                        url: 'https://github.com/rastikerdar/sahel-font',
                         type: 'sans-serif',
                         weights: ['normal', 'bold', 'black']
                     },
                     {
                         nameFa: 'صمیم',
                         nameEn: 'Samim',
+                        url: 'https://github.com/rastikerdar/samim-font',
                         type: 'sans-serif',
                         weights: ['normal', 'bold']
                     },
                     {
                         nameFa: 'شبنم',
                         nameEn: 'Shabnam',
+                        url: 'https://github.com/rastikerdar/shabnam-font',
                         type: 'sans-serif',
                         weights: ['normal', 'bold']
                     },
                     {
                         nameFa: 'وزیر',
                         nameEn: 'Vazir',
+                        url: 'https://github.com/rastikerdar/vazir-font',
                         type: 'sans-serif',
                         weights: ['ultra-light', 'normal', 'medium', 'bold']
                     },
                     {
                         nameFa: 'ویژه آزاد',
                         nameEn: 'Vizheh Azad',
+                        url: 'https://github.com/font-store/font-VizhehAzad',
                         type: 'serif',
                         weights: ['normal']
                     },
                     {
                         nameFa: 'زیرو سنس',
                         nameEn: 'Xero Sans',
+                        url: '',
                         type: 'sans-serif',
                         weights: ['normal']
                     }
@@ -235,8 +251,16 @@
         margin-bottom: 2em;
     }
 
-    .panel-heading .pull-left {
-        font-size: 0;
+    .panel-title {
+        white-space: nowrap;
+    }
+
+    .panel-title a {
+        color: #337ab7;
+    }
+
+    .panel-title a:hover {
+        color: #23527c;
     }
 
     .ultra-light {
